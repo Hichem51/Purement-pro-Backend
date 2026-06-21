17d7a4fce2d22dfb91d10133ce6cb226d7101447
Hichem51 <hichemmca401@hotmail.fr>
2026-06-21 09:53:10 +0100

    Prepare backend for safe GitHub push

--- dist/middlewares/not-found.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundMiddleware = void 0;
-const api_error_1 = require("../utils/api-error");
-const notFoundMiddleware = (req, _res, next) => {
-    next(new api_error_1.ApiError(404, `Route not found: ${req.originalUrl}`));
-};
-exports.notFoundMiddleware = notFoundMiddleware;
